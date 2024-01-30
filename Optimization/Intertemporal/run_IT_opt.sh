92f853f3ef9a5c17b6ad5037e8938d7cdc0280c1
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2024-01-30 10:41:09 -0500

    Update run_IT_opt.sh

--- Optimization/Intertemporal/run_IT_opt.sh
+++ Optimization/Intertemporal/run_IT_opt.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-#PBS -l walltime=1:00:00
-#PBS -l nodes=4:ppn=16
-#PBS -j oe
+#SBATCH -D /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/Optimization/Intertemporal
+#SBATCH -o /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/Optimization/Intertemporal/output/job.%j.out   # Name of the output file (eg. myMPI.oJobID)
+#SBATCH -e /storage/home/aas6791/work/LakeProblem/Lake_Problem_DPS/Optimization/Intertemporal/output/error.%j.out
+#SBATCH --nodes=3
+#SBATCH --ntasks-per-node=20
+#SBATCH --ntasks=60
+#SBATCH --exclusive
+#SBATCH --mem-per-cpu=10gb
+#SBATCH --partition=sla-prio           															# Queue name "parallel"
+#SBATCH --account=azh5924_b  														        		# allocation name
+#SBATCH --time=02:00:00       											 				      	# Run time (hh:mm:ss) - up to 36 hours
+#SBATCH --mail-user=aas6791@psu.edu             				 						# address for email notification   
 
-cd $PBS_O_WORKDIR
+module load openmpi/4.1.4
+module load boost/1.77.0
 
 # Your commands go here
 # arguments are <seed> <NFE>
 for i in {1..50}
 do
-  mpirun ./LakeITparallel $i 200000
+  srun ./LakeITparallel $i 200000
 done
+
+ 
