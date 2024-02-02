507f26a2a28bd26c74083ac8e411da5e6f197147
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2024-02-02 14:31:53 -0500

    Update dtlz2_paralleltest.sh

--- Optimization/borg/dtlz2_paralleltest.sh
+++ Optimization/borg/dtlz2_paralleltest.sh
@@ -3,7 +3,7 @@
 #SBATCH -o /storage/home/aas6791/scratch/Lake_Problem_DPS/borg/test.%j.out   # Name of the output file (eg. myMPI.oJobID)
 #SBATCH -e /storage/home/aas6791/scratch/Lake_Problem_DPS/borg/error.%j.out
 #SBATCH --nodes=4
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-node=4
 #SBATCH --exclusive
 #SBATCH --partition=sla-prio           															# Queue name "parallel"
 #SBATCH --account=azh5924_b  														        		# allocation name
