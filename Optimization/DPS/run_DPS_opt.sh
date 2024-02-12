77be3d6d6b06c269e8eca63d63be4de4c59e1d70
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2024-02-12 14:29:35 -0500

    Update run_DPS_opt.sh

--- Optimization/DPS/run_DPS_opt.sh
+++ Optimization/DPS/run_DPS_opt.sh
@@ -2,13 +2,14 @@
 #SBATCH -D /storage/home/aas6791/scratch/Lake_Problem_DPS/Optimization/DPS
 #SBATCH -e /storage/home/aas6791/scratch/Lake_Problem_DPS/Optimization/DPS/output/error.%j.out
 ##SBATCH -o /storage/home/aas6791/scratch/Lake_Problem_DPS/Optimization/DPS/output/output.%j.out
-#SBATCH --nodes=2
+#SBATCH --nodes=3
 #SBATCH --ntasks-per-node=20
+#SBATCH --ntasks=60
 #SBATCH --exclusive
-##SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=10
 #SBATCH --partition=sla-prio           															# Queue name "parallel"
 #SBATCH --account=azh5924_b  														        		# allocation name
-#SBATCH --time=00:05:00       											 				      	# Run time (hh:mm:ss) - up to 36 hours
+#SBATCH --time=01:00:00       											 				      	# Run time (hh:mm:ss) - up to 36 hours
 #SBATCH --mail-user=aas6791@psu.edu             				 						# address for email notification
 #SBATCH --mail-type=ALL                  									    			# email at Begin and End of job
 
