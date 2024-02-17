bf863f9f630d79aef8527068f06f3160bee09888
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2024-02-16 21:22:23 -0500

    comment out lines 3 and 4

--- Optimization/find_refSets.sh
+++ Optimization/find_refSets.sh
@@ -3,8 +3,8 @@
 source /etc/profile.d/modules.sh
 module load python
 
-python pareto.py ./DPS/sets/*.set -o 6-9 -e 0.01 0.01 0.001 0.001 --output DPS.resultfile --delimiter=" " --comment="#"
-cut -d ' ' -f 7-10 DPS.resultfile >DPS.reference
+#python pareto.py ./DPS/sets/*.set -o 6-9 -e 0.01 0.01 0.001 0.001 --output DPS.resultfile --delimiter=" " --comment="#"
+#cut -d ' ' -f 7-10 DPS.resultfile >DPS.reference
 
 python pareto.py ./Intertemporal/sets/*.set -o 100-103 -e 0.01 0.01 0.001 0.001 --output Intertemporal.resultfile --delimiter=" " --comment="#"
 cut -d ' ' -f 101-104 Intertemporal.resultfile >Intertemporal.reference
